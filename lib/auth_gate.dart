b15a27c0bb3dd588c284f6fdea62ec730cc28519
Romain Laboisse <labrom@gmail.com>
2025-01-23 16:56:18 -0800

    Make AuthGate use Riverpod

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -2,8 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthGate extends StatelessWidget {
+import 'auth.dart';
+
+class AuthGate extends ConsumerWidget {
   final Widget child;
   const AuthGate({
     super.key,
@@ -11,14 +14,21 @@ class AuthGate extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
+  Widget build(BuildContext context, WidgetRef ref) =>
+      StreamBuilder<User?>(
+      stream: ref.watch(authStateChangesProvider) as Stream<User?>,
       builder: (context, snapshot) => snapshot.hasData
           ? child
           : SignInScreen(
-              providers: [GoogleProvider(clientId: '')],
+              providers: [
+                GoogleProvider(
+                  // clientId not used on Android and iOS (iOSPreferPlist is true for iOS)
+                  clientId: '',
+                  iOSPreferPlist: true,
+                )
+              ],
             ),
     );
-  }
+
+
 }
